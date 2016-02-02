7a6851db81d13c59609770dcda98dae33cb7629f
jcaixinha <jcaixinha@realtime.co>
2016-02-02 13:23:26 +0000

    total swift conversion

---
+++ RTMChatSwift/MessageTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MessageTableViewCell.swift
+//  RTMChatSwift
+//
+//  Created by joao caixinha on 02/02/16.
+//  Copyright © 2016 Internet Business Technologies. All rights reserved.
+//
+
+import Foundation
